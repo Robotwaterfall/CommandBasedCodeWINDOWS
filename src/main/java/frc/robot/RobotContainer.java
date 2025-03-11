75c45ff69b6fc41bedb63a53b7b99a90f603cf4b
Jayson <jaysonwelshman@gmail.com>
2025-03-10 22:29:57 -0400

    Implemented an autonomous mode.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.OIConstants;
@@ -42,6 +44,11 @@ public class RobotContainer {
 
 
   public Command getAutonomousCommand() {
+    new SequentialCommandGroup(
+      new Intakecmd(intakesub, IntakeConstants.kIntakeSpeed)); //intake
+      
+      new ParallelCommandGroup(
+         new Intakecmd(intakesub, IntakeConstants.kOuttakeSpeed)); //outtake
     return null;
   }
 
